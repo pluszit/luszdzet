f6da5fc7260e0c1169b88f280de2a0249f1cf952
przemyslawl-c <przemyslawl-c@herbalife.com>
2025-01-12 18:13:35 +0100

    homepage 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {AuthProvider} from "./auth/AuthProvider.tsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import {PrivateRoute} from "./auth/PrivateRoute.tsx";
 import {Login} from "./login/Login.tsx";
 import {Main} from "./main/Main.tsx";
@@ -9,7 +9,7 @@ function App() {
 
     return (
         <AuthProvider>
-            <BrowserRouter>
+            <HashRouter basename="/">
                 <Routes>
                     <Route path="/luszdzet/login" element={<Login />} />
                     <Route
@@ -22,7 +22,7 @@ function App() {
                     />
                     **<Route path="*" element={<Login />} />**
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </AuthProvider>
     );
 }
