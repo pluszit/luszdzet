3c8d31fe4a9ccb844550f225354b8bd81a97640e
przemyslawl-c <przemyslawl-c@herbalife.com>
2025-01-12 18:08:23 +0100

    homepage 4

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/luszdzet/login',
+  base: '/luszdzet',
   plugins: [react()],
   build: {
     outDir: 'build'
